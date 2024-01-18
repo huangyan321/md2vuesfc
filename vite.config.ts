d2504450841bd3f6634389a686439a20f9d95eaf
huangyan321 <1604549268@qq.com>
2024-01-18 17:42:34 +0800

    build:external

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,14 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: [
+        'vue',
+        'markdown-it',
+        '@mdit-vue/plugin-sfc',
+        '@mdit-vue/plugin-component',
+        'hash-sum',
+        '@vue/compiler-sfc',
+      ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
